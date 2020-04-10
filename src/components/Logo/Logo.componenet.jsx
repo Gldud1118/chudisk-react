320ded840aa9facde4b012bc88cea64b73638768
이희영 <gl171111@gmail.com>
2020-04-10 10:13:10 +0900

    로고 컴포넌트 생성

---
+++ src/components/Logo/Logo.componenet.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { LogoContainer, LogoHead, LogoWrapper } from './Logo.styles';
+
+const Logo = () => {
+  return (
+    <LogoWrapper to='/disk/my-disk' className='logo'>
+      <LogoHead>chu</LogoHead>disk
+    </LogoWrapper>
+  );
+};
+
+export default Logo;
---
+++ src/components/Logo/Logo.styles.jsx
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+
+export const LogoHead = styled.span`
+  color: #031a61;
+  font-weight: 500;
+`;
+
+export const LogoWrapper = styled(Link)`
+  line-height: 59px;
+  font-size: 26px;
+  color: #0168fa;
+  font-weight: 400;
+`;
